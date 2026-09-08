437456921f2e74a82b25e15b2cfcecf6522c269a
jinseok <tlsql031221@gmail.com>
2026-09-08 23:34:26 +0900

    feat: init project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">라커룸 프로젝트 세팅 완료!</h1>
+    </div>
+  )
+}
+
+export default App;
